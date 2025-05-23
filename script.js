d3537ae82c81f912235670e5a3d84ea6d5383032
Md.Salah uddin <salauddincse96@gmail.com>
2025-05-23 10:17:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let choice=document.querySelectorAll(".choice");
 let msg=document.getElementById("msg");
 let playerWin=document.getElementById("player");
 let compWin=document.getElementById("comp");
-let count=0;
-let count2=0;
+let count=1;
+let count2=1;
 choice.forEach(event => {
     event.addEventListener("click",()=>{
         let comp=Math.floor(Math.random()*choice.length);
@@ -12,19 +12,42 @@ choice.forEach(event => {
         if(playerId==="rock"&&compId==="scissor"||playerId==="scissor"&&compId==="rock"){
             if(playerId==="rock"){
                 playerWin.innerHTML=`${count++}`;
+                msg.style.backgroundColor="green";
+                msg.innerText=`You win! ${playerId} beats ${compId}`;
             }
-            else if(compId==="rock"){
-                compWin.innerHTML=`${count2++}`;
+            else{
+                compWin.innerText=`${count2++}`;
+                msg.style.backgroundColor="red";
+                msg.innerText=`You lost! ${compId} beats ${playerId}`;
             }
         }
         else if(playerId==="scissor"&&compId==="paper"||playerId==="paper"&&compId==="scissor"){
-            msg.innerText="scissor win";
+            if(playerId==="scissor"){
+                playerWin.innerHTML=`${count++}`;
+                msg.style.backgroundColor="green";
+                msg.innerText=`You win! ${playerId} beats ${compId}`;
+            }
+            else {
+                compWin.innerText=`${count2++}`;
+                msg.style.backgroundColor="red";
+                msg.innerText=`You lost! ${compId} beats ${playerId}`;
+            }
         }
         else if(playerId==="paper"&&compId==="rock"||playerId==="rock"&&compId==="paper"){
-            msg.innerText="paper win";
+            if(playerId==="paper"){
+                playerWin.innerHTML=`${count++}`;
+                msg.style.backgroundColor="green";
+                msg.innerText=`You win! ${playerId} beats ${compId}`;
+            }
+            else {
+                compWin.innerText=`${count2++}`;
+                msg.style.backgroundColor="red";
+                msg.innerText=`You lost! ${compId} beats ${playerId}`;
+            }
         }
-        else if(playerId===compId){
-            msg.innerText="Same";
+        else{
+            msg.innerText="Its Draw!";
+            msg.style.backgroundColor="rgb(9, 9, 66)";
         }
     })
 });
